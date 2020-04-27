4071212012d97bb404d60e99ab9c151c807c450d
saurav0705 <sauravaggarwal98@gmail.com>
2020-04-27 17:02:39 +0530

    complete with api integration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import Content from './components/Content/Content';
-import Data from './test.json';
+import LoadingData from './components/LoadingData/LoadingData';
+import {URL} from './service';
+// import Data from './test.json';
 
 function App() {
-  const [play,setPlay]= useState(false);
-  return (
+  const [data,setData] = useState('');
+  const [loading,setLoading] = useState(0);
+  useEffect(()=>{
+    setInterval(()=> setLoading(loading => {if(loading=== 100){return 100;}else{ return loading+1}}),100);
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(resp => {setData(resp)}).catch(err => setData({"error":err}))
+
     
+  },[])
+  const [play,setPlay]= useState(false);
+  return (<>
+    {data.length!==0 ? 
      <Router>
        <Navigation start={()=> setPlay(true)} play={play}/>
-        <Content play={play} data={Data} stop={()=> {console.log('worked');setPlay(false)}}/>
-     </Router>
+        <Content play={play} data={data} stop={()=> {console.log('worked');setPlay(false)}}/>
+     </Router>:
+      <LoadingData loading={loading}/>}</>
 
     
   );
